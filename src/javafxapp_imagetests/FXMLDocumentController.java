1648ac1a2cffc62813db8a6405e693a68bf09086
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-04-02 00:35:03 -0600

    Playing with Executors.

--- src/javafxapp_imagetests/FXMLDocumentController.java
+++ src/javafxapp_imagetests/FXMLDocumentController.java
@@ -9,6 +9,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
@@ -33,17 +37,24 @@ public class FXMLDocumentController implements Initializable {
     private FileInputStream inputStream;
     private FileInputStream inputStream1;
     private int toggle = 0;
+    private Runnable r1;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
+        r1 = () -> {System.out.println("Test");};
+        //ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+        
         if (toggle == 0) {
             toggle = 1;
             simpleImageView.setImage(twoImage);
+            //exec.scheduleAtFixedRate(r1, 0, 2, TimeUnit.SECONDS);
         } else {
             toggle = 0;
             simpleImageView.setImage(oneImage);
+            //exec = Executors.newScheduledThreadPool(0);
+            //exec.shutdown();
         }
         
     }
